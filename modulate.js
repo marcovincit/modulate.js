e5e5c16b33dec2c5552bce229c6a9a78538bc448
Marco Vincit <contato@marcovincit.com>
2019-06-27 14:23:49 -0300

    Function simplified and changed variable names

--- modulate.js
+++ modulate.js
@@ -1,29 +1,8 @@
-function modulate(value, rangeA, rangeB, limit) {
-    let fromHigh, fromLow, result, toHigh, toLow;
-    if (limit == null) {
-        limit = false;
-    }
-    fromLow = rangeA[0];
-    fromHigh = rangeA[1];
-    toLow = rangeB[0];
-    toHigh = rangeB[1];
-    result = toLow + (((value - fromLow) / (fromHigh - fromLow)) * (toHigh - toLow));
-    if(limit === true) {
-        if(toLow < toHigh) {
-            if(result < toLow) {
-                return toLow;
-            }
-            if(result > toHigh) {
-                return toHigh;
-            }
-        }else{
-            if(result > toLow) {
-                return toLow;
-            }
-            if(result < toHigh) {
-                return toHigh;
-            }
-        }
-    }
-    return result;
+function modulate({value, rangeA, rangeB, limit = true}) {
+    const rangeAInicial = rangeA[0],
+          rangeAFinal = rangeA[1],
+          rangeBInicial = rangeB[0],
+          rangeBFinal = rangeB[1],
+          result = rangeBInicial + (((value - rangeAInicial) / (rangeAFinal - rangeAInicial)) * (rangeBFinal - rangeBInicial))
+    return limit === true && result > rangeBFinal ? rangeBFinal : result
 }
