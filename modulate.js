18e2a0abf1aaf00e269fac958ee3cf16343b78ba
Marco Vincit <contato@marcovincit.com>
2019-06-28 16:05:44 -0300

    Update modulate.js

--- modulate.js
+++ modulate.js
@@ -1,8 +1,8 @@
 function modulate(input, inputRange, outputRange, limit = true) {
-  const inputRangeInicial = inputRange[0],
+  const inputRangeInitial = inputRange[0],
         inputRangeFinal = inputRange[1],
-        outputRangeInicial = outputRange[0],
+        outputRangeInitial = outputRange[0],
         outputRangeFinal = outputRange[1],
-        result = outputRangeInicial + (((input - inputRangeInicial) / (inputRangeFinal - inputRangeInicial)) * (outputRangeFinal - outputRangeInicial))
+        result = outputRangeInitial + (((input - inputRangeInitial) / (inputRangeFinal - inputRangeInitial)) * (outputRangeFinal - outputRangeInitial))
   return limit === true && result > outputRangeFinal ? outputRangeFinal : result
 }
