6ee5f188bb55aa2cec809bd7a3188b8c924fd7c4
Marco Vincit <contato@marcovincit.com>
2019-06-30 21:00:36 -0300

    simplified function and change if

--- modulate.js
+++ modulate.js
@@ -1,8 +1,5 @@
-function modulate(input, [inputRangeInitial, inputRangeFinal], [outputRangeInitial, outputRangeFinal], limit) {
-    let result = (input - inputRangeInitial) / (inputRangeFinal - inputRangeInitial) * (outputRangeFinal - outputRangeInitial) + outputRangeInitial
-    if (!limit) return result
-    return outputRangeInitial < outputRangeFinal ? constrain(result, outputRangeInitial, outputRangeFinal) : constrain(result, outputRangeFinal, outputRangeInitial)
-}
-function constrain(result, low, high) {
-    return Math.max(Math.min(result, high), low)
+function modulate(input, [inputRangeInitial, inputRangeFinal], [outputRangeInitial, outputRangeFinal], limit){
+    let result = (input - inputRangeInitial) / (inputRangeFinal - inputRangeInitial) * (outputRangeFinal - outputRangeInitial) + outputRangeInitial;
+    function constrain(input, inputRangeInitial, inputRangeFinal) { return Math.max(Math.min(input, inputRangeFinal), inputRangeInitial) }
+    return limit ? outputRangeInitial < outputRangeFinal ? constrain(result, outputRangeInitial, outputRangeFinal) : constrain(result, outputRangeFinal, outputRangeInitial) : result
 }
